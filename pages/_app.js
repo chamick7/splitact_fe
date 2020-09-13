da3d03b34b3b8ee24d6fa3b1125850fbb590a476
chamick7 <sw.chal7.dev@gmail.com>
2020-09-14 01:18:50 +0700

    _app.js: add initial-scale=1.0

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot >
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <title>Split Act</title>
       </Head>
       <Navbar />
