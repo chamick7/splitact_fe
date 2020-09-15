2b94587aef497cb4e649d7db3c27220c98cbd009
miwwie02 <61011382@kmitl.ac.th>
2020-09-15 19:14:35 +0700

    home update

--- pages/index.js
+++ pages/index.js
@@ -40,11 +40,36 @@ export default function Home({}) {
 
       <div className={styles.recommend_container}>
         <section className={styles.recommend_container_left}>
-            <h1>1111</h1>
+          <img src="/img/calendar-preview.PNG" alt=""/>
         </section>
         <section className={styles.recommend_container_right}>
-            <h1>1111</h1>
+          <h1><span>Spilt ACT,</span>work with your team just got easier. </h1> 
+          <h1>By dividing work for each person and monitoring the work and</h1>
+          <h1> showing the progess of each part that the team members are responsible</h1>
         </section>
+      </div>
+      
+      <div className={styles.recommend_container}>
+        <section className={styles.recommend_container_two_left}>
+        <h1>use <span>'filter'</span> to categorize and prioritize you.</h1>
+        <h1>You can categorize tasks. And organize the sequence of </h1>
+        <h1>tasks that must be performed according to the needs</h1>
+        </section>
+        <section className={styles.recommend_container_two_right}>
+
+        </section>
+      </div>
+
+      <div className={styles.endpage}>
+      <section className={styles.calendar_container_right} >
+          <h1>Join to Split <span>A</span><span>C</span><span>T.</span></h1>
+          <label className={styles.home_email_input} htmlFor="email">
+            {/* <span>Get start</span> */}
+            <input type="email" name="email" placeholder="E-mail" />
+            <button>Free - Register</button>
+          </label>        
+      </section>
+      <img src="/img/join.png" alt="" />
       </div>
 
     </div>
