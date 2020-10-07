a635f97d5ce1379fbaa99360cb72f9ed5c5b840a
miwwie02 <61011382@kmitl.ac.th>
2020-10-07 20:21:47 +0700

    resetpassword success : responsive

--- pages/resetpassword/success.js
+++ pages/resetpassword/success.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Footer_min from "../../Components/Footer_min";
+
+import styles from "../../css/resetsuccess.module.css"
 
 export default function resetSuccess() {
     return (
-        <div>
-            <h1>Reset Success</h1>
+        <div className={styles.resetsuccess_body}>
+            <div className={styles.header}>
+                <p className={styles.h1}>Password <span>Reset</span> Email Sent</p>
+                <p className={styles.h3}>An email has been sent to your rescue email address,
+                61011382@hotmail.com  Follow the directions in the email to rest password</p>
+            </div>
+            <div className={styles.container}>
+                <button className={styles.done}>Done</button>
+            </div>
+            <img src="/img/succcesss.png" className={styles.img}/>
+            <Footer_min />
         </div>
     )
 }
