05327799cf095f67ff94094af0c9b3619546e2bc
chamick7 <sw.chal7.dev@gmail.com>
2020-10-16 22:01:04 +0700

    Dropdown: Change DropItem

--- Components/Dropdown.js
+++ Components/Dropdown.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignInAlt, faPowerOff } from "@fortawesome/free-solid-svg-icons";
-import { faAddressBook } from "@fortawesome/free-regular-svg-icons";
+import { faSignInAlt, faPowerOff,faAddressCard,faUsers,faExclamationCircle,faInfoCircle,faPager } from "@fortawesome/free-solid-svg-icons";
+import { faComments,faEdit,faQuestionCircle,faAddressBook  } from "@fortawesome/free-regular-svg-icons";
 import { faUserAlt, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { accountAtom } from "../atom";
 import Cookies from "js-cookies";
 import { useRecoilState } from "recoil";
 
 import styles from "../css/dropdown.module.css";
-
 
 export default function Dropdown() {
   const node = useRef();
@@ -22,9 +21,59 @@ export default function Dropdown() {
     setAccount({});
   };
 
+  function DropProfile() {
+    if (account.name) {
+      return(
+      <li className={styles.profile} onClick={()=>setOpen(false)} >
+        <Link href="/" >
+          <a>
+            {account.name}
+          </a>
+        </Link>
+      </li>
+      )
+    } else {
+      return (
+      <li className={styles.profile} onClick={()=>setOpen(false)}  >
+        <Link  href="/login" >
+          <a>
+            <FontAwesomeIcon icon={faSignInAlt} />
+            SignIn or Register
+          </a>
+        </Link>
+      </li>
+      )
+    }
+  }
+
   function DropItem(props) {
+
+    if(props.type){
+      return (
+        <li
+        className={styles.dropitem}
+        onClick={(e) => {
+          handleLogout;
+          setOpen(false);
+        }}
+      >
+        <Link href={props.to}>
+          <a>
+            {" "}
+            <FontAwesomeIcon icon={props.icon} /> {props.word}
+          </a>
+        </Link>
+      </li>
+      )
+    } else
+
     return (
-      <li className={styles.dropitem+" "+props.class} onClick={(e) => {setOpen(false)}} >
+      <li
+        className={styles.dropitem}
+        onClick={(e) => {
+          setOpen(false);
+        }}
+      >
         <Link href={props.to}>
           <a>
             {" "}
@@ -35,40 +84,48 @@ export default function Dropdown() {
     );
   }
 
-  
   function DropdownMenu() {
     return (
       <div className={styles.dropdown}>
         <ul className={styles.dropmenu}>
-          {account.name ? (
-            //not Login
+          
+          <DropProfile />
+
+          {
+            account.name?
             <>
-              <li className={styles.dropitem,styles.logout_dropitem} onClick={handleLogout}>
-                {" "}
-                <FontAwesomeIcon
-                  className={styles.logout_icon}
-                  icon={faPowerOff}
-                />{" "}
-                Logout
-              </li>
-            </>
-          ) : (
-            //logined
+              <DropItem icon={faAddressCard} word="Profile" to="/profile" />
+              <DropItem icon={faComments} word="Chat" to="/chat" />
+            </>:<></>
+          }
+
+          <hr className={styles.hr} />
+          
+          {
+            account.name? 
             <>
-              <DropItem
-                class={styles.login_dropitem}
-                to="/login"
-                word="Login"
-                icon={faSignInAlt}
-              />
-              <DropItem
-                class={styles.register_dropitem}
-                to="/register"
-                word="Register"
-                icon={faAddressBook}
-              />
-            </>
-          )}
+              <DropItem icon={faEdit} word="Activity" to="/dashboard"  />
+              <DropItem icon={faUsers} word="Teams" to="/dashboard"  />
+
+              <hr className={styles.hr} />
+
+            </>:<></>
+          }
+
+          <DropItem icon={faQuestionCircle} word="Help" to="/help"  />
+          <DropItem icon={faExclamationCircle} word="Report" to="/report"  />
+          <DropItem icon={faAddressBook} word="Contact" to="/contact"  />
+          <DropItem icon={faPager} word="About" to="/about"  />
+
+          {
+            account.name?
+            <>
+            <hr className={styles.hr} />
+            <DropItem icon={faPowerOff} word="Sign out" type="logout" to="/"/>
+            </>:<></>
+          }
+
+          
         </ul>
       </div>
     );
@@ -77,7 +134,6 @@ export default function Dropdown() {
   const handleClickOutside = (e) => {
     if (node.current.contains(e.target)) {
       // inside click
-      console.log('click');
       return;
     }
     // outside click
@@ -85,17 +141,16 @@ export default function Dropdown() {
   };
 
   useEffect(() => {
-    
-      document.addEventListener("mousedown", handleClickOutside);
-      
+    document.addEventListener("mousedown", handleClickOutside);
+
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <button ref={node} className={styles.btn_nav}  >
-      <div  onClick={() => setOpen(!open)}  >
+    <button ref={node} className={styles.btn_nav}>
+      <div onClick={() => setOpen(!open)}>
         <FontAwesomeIcon className={styles.nav_icon1} icon={faUserAlt} />
         <span>{account.name ? account.name : "Member"}</span>
         <FontAwesomeIcon className={styles.nav_icon2} icon={faCaretDown} />
