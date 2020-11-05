0827c94b70be8b7c9d68fa62871ab57f0688e759
miwwie02 <61011382@kmitl.ac.th>
2020-11-05 12:44:53 +0700

    progressbar : tryhard

--- Components/Progressbar.js
+++ Components/Progressbar.js
@@ -4,21 +4,19 @@ import React from 'react'
 import styles from "../css/progressbar.module.css"
 
 export default function Progressbar(){
-    
-
 
     return(
         <ol className={styles.progress}>
-            <li className={styles.done}>
+            <li className={styles.active}>
                 <span className={styles.step}><span>1</span></span>
             </li>
-            <li className={styles.done}>
+            <li >
                 <span className={styles.step}><span>2</span></span>
             </li>
-            <li className={styles.active}>
+            <li >
                 <span className={styles.step}><span>3</span></span>
             </li>
-            <li>
+            <li >
                 <span className={styles.step}><span>4</span></span>
             </li>
         </ol>
