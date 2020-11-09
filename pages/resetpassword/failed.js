67b82de2a51a9e7b5c46b71ab06c095d54ad4e2c
miwwie02 <61011382@kmitl.ac.th>
2020-11-09 20:35:55 +0700

    failed : responsive

--- pages/resetpassword/failed.js
+++ pages/resetpassword/failed.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Footer_min from "../../Components/Footer_min";
+
 import styles from "../../css/resetpassword_failed.module.css"
 
 export default function resetpassword_failed(){
@@ -9,7 +11,12 @@ export default function resetpassword_failed(){
         <h1>Password Reset <span>Failed</span></h1>
         <h3>it was not possible to reset your password.</h3>
         <h3>This may happen if your token is invalid or expired.</h3>
-        <button></button>
+        <div className={styles.button_container}>
+            <button>Do it again</button>
+            <button>Back to login</button>
+        </div>
+        <img src="/img/504error.png" alt=""/>
+        <Footer_min/>
     </div>
     
     );
