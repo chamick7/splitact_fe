ec5dd1010b9b7b5b2144c53d3e5c7eaed731b59d
chamick7 <sw.chal7.dev@gmail.com>
2020-11-16 11:06:21 +0700

    activityModal: adjust responsive

--- pages/activity.js
+++ pages/activity.js
@@ -1,23 +1,15 @@
 import { useEffect } from "react";
-import { getAxios }  from "../utils/axios";
+import { getAxios } from "../utils/axios";
 import ProtectRoute from "../utils/ProtectRoute";
 
 export default function activity() {
   const axios = getAxios();
 
-//   useEffect(() => {
-//     axios
-//       .post("/activity",{
-//           atName: "Test activity",
-//           members: ['5f916088382807330879eb65']
-//       })
-//       .then((res) => {
-//         console.log(res.data);
-//       })
-//       .catch(err => {
-//           console.log(err.response);
-//       });
-//   }, []);
+  useEffect(() => {
+    
+  }, [])
 
-  return <ProtectRoute></ProtectRoute>;
+  return <ProtectRoute>
+    <h1>Hello</h1>
+  </ProtectRoute>;
 }
